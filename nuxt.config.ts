94cd932d6f0fc39378ff6716c100cac4257cf90d
Orhan Saeedi <orhan.saeedi@bs.ch>
2025-12-05 10:57:31 +0100

    Remove unused PWA icon configurations from nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -67,16 +67,6 @@ export default defineNuxtConfig({
       display: 'standalone',
       background_color: '#ffffff',
       theme_color: '#00838f',
-      icons: [
-        { src: '/icons/pwa-192x192.png', sizes: '192x192', type: 'image/png' },
-        { src: '/icons/pwa-512x512.png', sizes: '512x512', type: 'image/png' },
-        {
-          src: '/icons/pwa-maskable-512x512.png',
-          sizes: '512x512',
-          type: 'image/png',
-          purpose: 'maskable',
-        },
-      ],
     },
   },
 
